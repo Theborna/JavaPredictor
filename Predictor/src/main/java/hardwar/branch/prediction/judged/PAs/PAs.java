63906135c39bcdbe4fafed5e972ab3583efb3d6c
Theborna <borna710@gmail.com>
2023-06-01 16:37:28 +0330

    PAs

--- Predictor/src/main/java/hardwar/branch/prediction/judged/PAs/PAs.java
+++ Predictor/src/main/java/hardwar/branch/prediction/judged/PAs/PAs.java
@@ -1,6 +1,5 @@
 package hardwar.branch.prediction.judged.PAs;
 
-
 import hardwar.branch.prediction.shared.*;
 import hardwar.branch.prediction.shared.devices.*;
 
@@ -20,24 +19,26 @@ public class PAs implements BranchPredictor {
     }
 
     public PAs(int BHRSize, int SCSize, int branchInstructionSize, int KSize, HashMode hashMode) {
-        // TODO: complete the constructor
-        this.branchInstructionSize = 0;
-        this.KSize = 0;
-        this.hashMode = HashMode.XOR;
+        this.branchInstructionSize = branchInstructionSize;
+        this.KSize = KSize;
+        this.hashMode = hashMode;
 
         // Initialize the PABHR with the given bhr and branch instruction size
-        PABHR = null;
+        PABHR = new RegisterBank(branchInstructionSize, BHRSize);
 
-        // Initializing the PAPHT with K bit as PHT selector and 2^BHRSize row as each PHT entries
+        // Initializing the PAPHT with K bit as PHT selector and 2^BHRSize row as each
+        // PHT entries
         // number and SCSize as block size
-        PSPHT = null;
+        PSPHT = new PerAddressPredictionHistoryTable(
+                KSize, 1 << BHRSize, SCSize);
 
         // Initialize the saturating counter
-        SC = null;
+        this.SC = new SIPORegister("SC", SCSize, null);
     }
 
     /**
-     * predicts the result of a branch instruction based on the per address BHR and hash value of branch
+     * predicts the result of a branch instruction based on the per address BHR and
+     * hash value of branch
      * instruction address
      *
      * @param branchInstruction the branch instruction
@@ -45,13 +46,35 @@ public class PAs implements BranchPredictor {
      */
     @Override
     public BranchResult predict(BranchInstruction branchInstruction) {
-        // TODO: complete Task 1
-        return BranchResult.NOT_TAKEN;
+        // get BHR value
+        ShiftRegister BHR = PABHR.read(branchInstruction.getInstructionAddress());
+        // concatenate the instruction address
+        // hashing the address
+        Bit[] hash = getCacheEntry(branchInstruction.getInstructionAddress(), BHR.read());
+
+        // initialize the PHT if empty
+        PSPHT.putIfAbsent(hash, getDefaultBlock());
+        // read from the cache
+        Bit[] block = PSPHT.get(hash);
+        // load into the SC register
+        SC.load(block);
+        return BranchResult.of(block[0].getValue());
     }
 
     @Override
     public void update(BranchInstruction instruction, BranchResult actual) {
-        // TODO:complete Task 2
+        // counting from the SC register
+        Bit[] counted = CombinationalLogic.count(this.SC.read(),
+                BranchResult.isTaken(actual), CountMode.SATURATING);
+        // updating our cache
+        // get BHR value
+        ShiftRegister BHR = PABHR.read(instruction.getInstructionAddress());
+        Bit[] hash = getCacheEntry(instruction.getInstructionAddress(), BHR.read());
+
+        PSPHT.put(hash, counted);
+        // updating the BHR
+        BHR.insert(Bit.of(BranchResult.isTaken(actual)));
+        PABHR.write(instruction.getInstructionAddress(), BHR.read());
     }
 
     @Override
@@ -70,7 +93,6 @@ public class PAs implements BranchPredictor {
 
         return cacheEntry;
     }
-
 
     private Bit[] getDefaultBlock() {
         Bit[] defaultBlock = new Bit[SC.getLength()];
